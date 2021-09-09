8497eddd09418593b17ca652bd13619a58a5c88b
Chris Johnston <christopherjohnston@protonmail.com>
2021-09-09 21:44:52 +0100

    amended READme

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # CV Creator
 
-A React app which allows users to create a CV using a template and export it as a PDF.
+A React app which allows users to create a CV using a template and export it as a PDF. This web-app makes heavy use of React Hooks to manage various states. In particular, the useReducer Hook is heavily utilised to trigger different things depending on the the arguments passed to the reducers. I found that reducers were the most suitable Hook for this project due to the sheer amount of different states the CV entries could have (e.g. edited, deleted). Due to the small-scale of the project, I felt it was more appropriate to use useReducer and the ContextAPI than the Redux library.
+
+SCSS is used to neatly seperate out the CSS.
+
+## Reflection
+
+Whilst I am pleased with the final result, on reflection I would opt for a more visually appealing design as currently it is quite spartan, which impacts usability.
 
 ## Getting started
 
@@ -12,6 +18,7 @@ npm start
 ```
 
 ## Built with
-* [React](https://reactjs.org/)
-* [ReactToPrint](https://www.npmjs.com/package/react-to-print)
-* [uniqid](https://www.npmjs.com/package/uniqid)
+
+- [React](https://reactjs.org/)
+- [ReactToPrint](https://www.npmjs.com/package/react-to-print) - To allow users to save the CV as a PDF.
+- [uniqid](https://www.npmjs.com/package/uniqid)
--- src/components/reducers.js
+++ src/components/reducers.js
@@ -1,179 +1,177 @@
-// state reducer functions fo
+// state reducer functions
 const generalInfoReducer = (state, action) => {
-  switch (action.type) {
-    case "HANDLE_GENINFO_TEXT":
-      return {
-        ...state,
-        [action.field]: action.payload,
-      };
-    case "GENINFO_SUBMIT":
-      return {
-        ...state,
-        data: [action.payload],
-        isEdit: false,
-        isDelete: false,
-        name: "",
-        email: "",
-        mobile: "",
-        city: "",
-        summary: "",
-      };
-    case "GENINFO_EDIT":
-      const newArray = [...state.data];
-      const index = newArray.findIndex((d) => d.id === action.payload.id);
-      newArray[index].isEdit = true;
-      return {
-        ...state,
-        data: newArray,
-      };
-    case "GENINFO_DELETE":
-      return {
-        ...state,
-        isEdit: false,
-        isDelete: true,
-        data: state.data.filter((geninfo) => action.payload.id !== geninfo.id),
-      };
-    case "GENINFO_UPDATE_SAVE": {
-      const newArray = [...state.data];
-      const index = newArray.findIndex((d) => d.id === action.payload.id);
-      newArray[index].isEdit = false;
-      return {
-        ...state,
-        data: newArray,
-      };
-    }
-    case "GENINFO_UPDATE": {
-      const newArray = [...state.data];
-      const index = newArray.findIndex((d) => action.id === d.id);
-      newArray[index][action.field] = action.payload;
-      return {
-        ...state,
-        ...state.data,
-        data: newArray,
-      };
-    }
-    default:
-      throw new Error();
-  }
+	switch (action.type) {
+		case "HANDLE_GENINFO_TEXT":
+			return {
+				...state,
+				[action.field]: action.payload,
+			};
+		case "GENINFO_SUBMIT":
+			return {
+				...state,
+				data: [action.payload],
+				isEdit: false,
+				isDelete: false,
+				name: "",
+				email: "",
+				mobile: "",
+				city: "",
+				summary: "",
+			};
+		case "GENINFO_EDIT":
+			const newArray = [...state.data];
+			const index = newArray.findIndex((d) => d.id === action.payload.id);
+			newArray[index].isEdit = true;
+			return {
+				...state,
+				data: newArray,
+			};
+		case "GENINFO_DELETE":
+			return {
+				...state,
+				isEdit: false,
+				isDelete: true,
+				data: state.data.filter((geninfo) => action.payload.id !== geninfo.id),
+			};
+		case "GENINFO_UPDATE_SAVE": {
+			const newArray = [...state.data];
+			const index = newArray.findIndex((d) => d.id === action.payload.id);
+			newArray[index].isEdit = false;
+			return {
+				...state,
+				data: newArray,
+			};
+		}
+		case "GENINFO_UPDATE": {
+			const newArray = [...state.data];
+			const index = newArray.findIndex((d) => action.id === d.id);
+			newArray[index][action.field] = action.payload;
+			return {
+				...state,
+				...state.data,
+				data: newArray,
+			};
+		}
+		default:
+			throw new Error();
+	}
 };
 
 const jobsReducer = (state, action) => {
-  switch (action.type) {
-    case "HANDLE_JOBS_TEXT":
-      return {
-        ...state,
-        [action.field]: action.payload,
-      };
-    case "JOBS_SUBMIT":
-      return {
-        ...state,
-        data: state.data.concat([action.payload]),
-        isEdit: false,
-        isDelete: false,
-        company: "",
-        position: "",
-        startDate: "",
-        endDate: "",
-      };
-    case "JOBS_EDIT":
-      const newArray = [...state.data];
-      const index = newArray.findIndex((d) => d.id === action.payload.id);
-      newArray[index].isEdit = true;
-      return {
-        ...state,
-        data: newArray,
-      };
+	switch (action.type) {
+		case "HANDLE_JOBS_TEXT":
+			return {
+				...state,
+				[action.field]: action.payload,
+			};
+		case "JOBS_SUBMIT":
+			return {
+				...state,
+				data: state.data.concat([action.payload]),
+				isEdit: false,
+				isDelete: false,
+				company: "",
+				position: "",
+				startDate: "",
+				endDate: "",
+			};
+		case "JOBS_EDIT":
+			const newArray = [...state.data];
+			const index = newArray.findIndex((d) => d.id === action.payload.id);
+			newArray[index].isEdit = true;
+			return {
+				...state,
+				data: newArray,
+			};
 
-    case "JOBS_DELETE":
-      return {
-        ...state,
-        isEdit: false,
-        isDelete: true,
-        data: state.data.filter((job) => action.payload.id !== job.id),
-      };
-    case "JOBS_UPDATE_SAVE": {
-      const newArray = [...state.data];
-      const index = newArray.findIndex((d) => d.id === action.payload.id);
-      newArray[index].isEdit = false;
-      return {
-        ...state,
-        data: newArray,
-      };
-    }
-    case "JOBS_UPDATE": {
-      const newArray = [...state.data];
-      const index = newArray.findIndex((d) => action.id === d.id);
-      newArray[index][action.field] = action.payload;
-      return {
-        ...state,
-        ...state.data,
-        data: newArray,
-      };
-    }
-    default:
-      throw new Error();
-  }
+		case "JOBS_DELETE":
+			return {
+				...state,
+				isEdit: false,
+				isDelete: true,
+				data: state.data.filter((job) => action.payload.id !== job.id),
+			};
+		case "JOBS_UPDATE_SAVE": {
+			const newArray = [...state.data];
+			const index = newArray.findIndex((d) => d.id === action.payload.id);
+			newArray[index].isEdit = false;
+			return {
+				...state,
+				data: newArray,
+			};
+		}
+		case "JOBS_UPDATE": {
+			const newArray = [...state.data];
+			const index = newArray.findIndex((d) => action.id === d.id);
+			newArray[index][action.field] = action.payload;
+			return {
+				...state,
+				...state.data,
+				data: newArray,
+			};
+		}
+		default:
+			throw new Error();
+	}
 };
 
 const educationReducer = (state, action) => {
-  switch (action.type) {
-    case "HANDLE_EDUCATION_TEXT":
-      return {
-        ...state,
-        [action.field]: action.payload,
-      };
-    case "EDUCATION_SUBMIT":
-      return {
-        ...state,
-        data: state.data.concat([action.payload]),
-        isEdit: false,
-        isDelete: false,
-        instName: "",
-        grades: "",
-        startDate: "",
-        endDate: "",
-      };
-    case "EDUCATION_EDIT":
-      const newArray = [...state.data];
-      const index = newArray.findIndex((d) => d.id === action.payload.id);
-      newArray[index].isEdit = true;
-      return {
-        ...state,
-        data: newArray,
-      };
-    case "EDUCATION_DELETE":
-      return {
-        ...state,
-        isEdit: false,
-        isDelete: true,
-        data: state.data.filter(
-          (education) => action.payload.id !== education.id
-        ),
-      };
-    case "EDUCATION_UPDATE_SAVE": {
-      const newArray = [...state.data];
-      const index = newArray.findIndex((d) => d.id === action.payload.id);
-      newArray[index].isEdit = false;
-      return {
-        ...state,
-        data: newArray,
-      };
-    }
-    case "EDUCATION_UPDATE": {
-      const newArray = [...state.data];
-      const index = newArray.findIndex((d) => action.id === d.id);
-      newArray[index][action.field] = action.payload;
-      return {
-        ...state,
-        ...state.data,
-        data: newArray,
-      };
-    }
-    default:
-      throw new Error();
-  }
+	switch (action.type) {
+		case "HANDLE_EDUCATION_TEXT":
+			return {
+				...state,
+				[action.field]: action.payload,
+			};
+		case "EDUCATION_SUBMIT":
+			return {
+				...state,
+				data: state.data.concat([action.payload]),
+				isEdit: false,
+				isDelete: false,
+				instName: "",
+				grades: "",
+				startDate: "",
+				endDate: "",
+			};
+		case "EDUCATION_EDIT":
+			const newArray = [...state.data];
+			const index = newArray.findIndex((d) => d.id === action.payload.id);
+			newArray[index].isEdit = true;
+			return {
+				...state,
+				data: newArray,
+			};
+		case "EDUCATION_DELETE":
+			return {
+				...state,
+				isEdit: false,
+				isDelete: true,
+				data: state.data.filter(
+					(education) => action.payload.id !== education.id
+				),
+			};
+		case "EDUCATION_UPDATE_SAVE": {
+			const newArray = [...state.data];
+			const index = newArray.findIndex((d) => d.id === action.payload.id);
+			newArray[index].isEdit = false;
+			return {
+				...state,
+				data: newArray,
+			};
+		}
+		case "EDUCATION_UPDATE": {
+			const newArray = [...state.data];
+			const index = newArray.findIndex((d) => action.id === d.id);
+			newArray[index][action.field] = action.payload;
+			return {
+				...state,
+				...state.data,
+				data: newArray,
+			};
+		}
+		default:
+			throw new Error();
+	}
 };
-
-
 
 export { generalInfoReducer, jobsReducer, educationReducer };
